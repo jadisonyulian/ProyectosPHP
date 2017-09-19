828f541222209c049e65259ba0fa4e182be1822c
jadisonyulian <jadisonyulian@gmail.com>
2017-09-19 06:39:05 -0500

    PRUEBAS 5

--- 07.CMS/cms_mvc/backend/index.php
+++ 07.CMS/cms_mvc/backend/index.php
@@ -11,6 +11,8 @@ require_once "controllers/enlaces.php";
 require_once "controllers/ingreso.php";
 echo "prueba";
 
+echo "prueba";
+
 $template = new TemplateController();
 $template -> template();
 ob_end_flush();
