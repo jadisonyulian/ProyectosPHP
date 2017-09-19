477aa1e1f4cb8dc8acca1dfddabc0242211a3a80
jadisonyulian <jadisonyulian@gmail.com>
2017-09-19 06:28:32 -0500

    pruebas

--- 07.CMS/cms_mvc/backend/index.php
+++ 07.CMS/cms_mvc/backend/index.php
@@ -9,7 +9,7 @@ require_once "models/ingreso.php";
 require_once "controllers/template.php";
 require_once "controllers/enlaces.php";
 require_once "controllers/ingreso.php";
-
+echo "prueba";
 
 $template = new TemplateController();
 $template -> template();
